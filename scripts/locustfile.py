717e9b4b30312c9159d0515812b29885cc954830
netosouzasoares <netosouzasoares11@gmail.com>
2022-03-24 14:22:40 -0300

    fix lint

--- scripts/locustfile.py
+++ scripts/locustfile.py
@@ -1,8 +1,24 @@
 from locust import HttpUser, task
 import random
 
+
 def get_requests():
-    return [{'price': 200.20}, {'price': 999.20}, {'price': 1.20},{'price': 500.20} ,{'price': 210.20} ,{'price': 20.20}, {'price': 0.05}, {'price': 1.20}, {'price': 0.20}, {'price': 30}, {'price': 1.20}, {'price': 2020.20},{'price': 10.20}]
+    return [
+        {'price': 200.20},
+        {'price': 999.20},
+        {'price': 1.20},
+        {'price': 500.20},
+        {'price': 210.20},
+        {'price': 20.20},
+        {'price': 0.05},
+        {'price': 1.20},
+        {'price': 0.20},
+        {'price': 30},
+        {'price': 1.20},
+        {'price': 2020.20},
+        {'price': 10.20}
+    ]
+
 
 class Converter(HttpUser):
     result = get_requests()
